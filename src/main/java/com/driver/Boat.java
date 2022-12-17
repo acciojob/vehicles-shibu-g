c6c31591c0f602b7164d9031031a4a098efbd376
Shibu Gope <shibugoper1@gmail.com>
2022-12-17 19:27:00 +0530

    hello

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,18 @@
+package com.driver;
+
+public class Boat implements WaterVehicle{
+
+	@Override
+	public String getVehicleName() {
+		// TODO Auto-generated method stub
+		return getVehicleName();
+	}
+
+	@Override
+	public int getVehicleCapacity() {
+		// TODO Auto-generated method stub
+		return getVehicleCapacity();
+	}
+	
+
+}
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,10 +4,14 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+    	super(name,2,3,4,isManual,"hi",5);
     }
 
     public void accelerate(int rate){
         int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        
+        newSpeed=getCurrentSpeed()+rate;
+        
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -19,8 +23,27 @@ public class F1 extends Car {
          */
 
         if(newSpeed == 0) {
+        	changeGear(1);
             //Stop the car, set gear as 1
         }
+        else if(newSpeed>0&&newSpeed<=50) {
+        	changeGear(1);
+        }
+        else if(newSpeed>50&&newSpeed<=100) {
+        	changeGear(2);
+        }
+        else if(newSpeed>100&&newSpeed<=150) {
+        	changeGear(3);
+        }
+        else if(newSpeed>150&&newSpeed<=200) {
+        	changeGear(4);
+        }
+        else if(newSpeed>200&&newSpeed<=250) {
+        	changeGear(5);
+        }else {
+        	changeGear(6);
+        }
+        
         //for all other cases, change the gear accordingly
 
         if(newSpeed > 0) {
